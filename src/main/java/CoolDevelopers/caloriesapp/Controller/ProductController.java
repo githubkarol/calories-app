cd7689c9ca511cbce74cf3f9ea6d2c60e1a2d8b8
githubdevernest <github_ernest@onet.pl>
2024-04-15 14:41:49 +0200

    added endpoint for single product return

--- src/main/java/CoolDevelopers/caloriesapp/Controller/ProductController.java
+++ src/main/java/CoolDevelopers/caloriesapp/Controller/ProductController.java
@@ -2,14 +2,17 @@ package CoolDevelopers.caloriesapp.Controller;
 
 import CoolDevelopers.caloriesapp.DAO.Product;
 import CoolDevelopers.caloriesapp.Service.ProductService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -21,5 +24,10 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
         return new ResponseEntity<List<Product>>(productService.allProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> getProduct(@PathVariable ObjectId id) {
+        return new ResponseEntity<Optional<Product>>(productService.getSingleProduct(id), HttpStatus.OK);
     }
 }
--- src/main/java/CoolDevelopers/caloriesapp/Service/ProductService.java
+++ src/main/java/CoolDevelopers/caloriesapp/Service/ProductService.java
@@ -2,10 +2,12 @@ package CoolDevelopers.caloriesapp.Service;
 
 import CoolDevelopers.caloriesapp.DAO.Product;
 import CoolDevelopers.caloriesapp.Repository.ProductRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -14,5 +16,9 @@ public class ProductService {
     private ProductRepository productRepository;
     public List<Product> allProducts() {
         return  productRepository.findAll();
+    }
+
+    public Optional<Product> getSingleProduct(ObjectId id) {
+        return productRepository.findById(id);
     }
 }
